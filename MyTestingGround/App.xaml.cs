87b4e7ee1d99b3b055dc15cdbf2a73591d386067
Justun Gao <justunyeyeye@gmail.com>
2024-06-06 10:23:42 -0400

    Add files

---
+++ MyTestingGround/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyTestingGround;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainWindowViewModel = new MainViewModel();
+        var mainWindow = new MainWindow
+        {
+            DataContext = mainWindowViewModel
+        };
+
+        MainWindow = mainWindow;
+        MainWindow.Show();
+
+    }
+}
+
---
+++ MyTestingGround/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PropertyChanged;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyTestingGround;
+
+
+[AddINotifyPropertyChangedInterface]
+public partial class MainViewModel
+{
+    public int MyProperty { get; set; } = 1;
+
+    [RelayCommand]
+    private void Increment() => ++MyProperty;
+}
